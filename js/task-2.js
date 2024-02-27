be3b38c98f2e514d6d28470c82c28ec05e16cfac
NatalySv <136372155+NatalySv@users.noreply.github.com>
2024-02-27 09:18:42 +0200

    Update task-2.js
    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -13,7 +13,9 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-    this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    if (this.#items.indexOf(itemToRemove) !== -1) {
+      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    }
   }
 }
 
@@ -22,4 +24,6 @@ console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+storage.removeItem('Prolongers');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
